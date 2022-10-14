6c87aaf1ec10068648f5f88e3cf8705d91c10c34
unknown <admin@Jasmeet-temp.sjc.ironsystems.com>
2022-10-14 14:37:52 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    <meta name="description" content="Iron Service Global website" />
+  </>
+)
+
+// Step 3: Export your component
+export default AboutPage
